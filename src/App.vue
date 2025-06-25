6fa90fa6dcdbfcc6d8a26399d66246fbddbe33f3
Zainul Ulum <shadow.mx350@gmail.com>
2025-06-25 20:37:46 +0700

    rapihkan App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import { RouterView } from "vue-router";
-import Navbar from "./components/Navbar.vue";
-import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-  <Navbar />
   <RouterView />
-  <Footer />
 </template>
 
 <style></style>
